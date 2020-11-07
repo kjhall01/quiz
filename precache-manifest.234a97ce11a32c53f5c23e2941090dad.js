fa4f5989ee99c81e3992a9d6477c911ecc0e3a5b
Kyle Hall <hallkjc01@gmail.com>
2020-11-07 15:30:50 -0500

    Updates

---
+++ precache-manifest.234a97ce11a32c53f5c23e2941090dad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83bc1a63d441326084bc9635b73f80c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "693763185f671d188aeb",
+    "url": "/static/css/2.904436ba.chunk.css"
+  },
+  {
+    "revision": "9fa26c74503a3f241358",
+    "url": "/static/css/main.e222fcf8.chunk.css"
+  },
+  {
+    "revision": "693763185f671d188aeb",
+    "url": "/static/js/2.7ddd65e7.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/static/js/2.7ddd65e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fa26c74503a3f241358",
+    "url": "/static/js/main.057fcb75.chunk.js"
+  },
+  {
+    "revision": "74ce78787374f9bc5d5d",
+    "url": "/static/js/runtime-main.d71caae3.js"
+  }
+]);
